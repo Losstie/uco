eb83191ab31aa89dcee6ba81e68da05788c42f47
losstie <losstie@outlook.com>
2020-12-07 21:47:13 +0800

    finish

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,66 +1,64 @@
 // pages/collect/collect.js
+import {request} from "../../request/index.js"
+import regeneratorRuntim, { AsyncIterator } from '../../lib/runtime/runtime'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    collect:[],
+    tabs:[
+      {
+        id:0,
+        value:'商品收藏',
+        isActive:true
+      },
+      {
+        id:1,
+        value:'品牌收藏',
+        isActive:false
+      },
+      {
+        id:2,
+        value:'店铺收藏',
+        isActive:false
+      },
+      {
+        id:3,
+        value:'浏览足迹',
+        isActive:false
+      }
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
+    const token = wx.getStorageSync('token');
+    if(!token){
+      wx.navigateTo({
+        url: '/pages/auth/auth',
+      });
+      return;
+    }
+    const collect = wx.getStorageSync('collect') || [];
+    this.setData({collect});
 
+    const pages = getCurrentPages();
+    let currentPage = pages[pages.length-1];
+    const {type} = currentPage.options;
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleitemChange(e){
+    const {index} = e.detail;
+    let {tabs} = this.data;
+    tabs.forEach(v=>{
+      v.isActive=v.id===index?true:false;
+    });
+    this.setData({
+      tabs  
+    });
+    this.changeTitleByIndex(index);
+    this.getOrders(index+1);
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,5 +1,7 @@
 // pages/goods_detail/goods_detail.js
 import {request} from "../../request/index"
+import regeneratorRuntim, { AsyncIterator } from '../../lib/runtime/runtime'
+
 Page({
 
   /**
@@ -7,7 +9,8 @@ Page({
    */
   data: {
     goods_id:'',
-    goodsObj:{}
+    goodsObj:{},
+    isCollect:false
 
     
   },
@@ -17,30 +20,44 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const { goods_id } = options;
-    this.setData({
-      goods_id
-    });
+  onShow: function () {
+    let pages = getCurrentPages();
+    let cur = pages[pages.length-1];
+    let options = cur.options;
+    const {goods_id} = options;
     this.getGoodsDetail(goods_id);
   },
   // 获取商品详情
-  getGoodsDetail(goods_id){
-    request({
-      url:'/goods/detail',
-      data:{goods_id}
-    }).then(res=>{
-      let _goodsObj = res.data.message;
-      this.goodsInfo = _goodsObj;
+  async getGoodsDetail(goods_id){
+  //   request({
+  //     url:'/goods/detail',
+  //     data:{goods_id}
+  //   }).then(res=>{
+  //     let _goodsObj = res.data.message;
+  //     this.goodsInfo = _goodsObj;
+  //     this.setData({
+  //       goodsObj:{
+  //         goods_name:_goodsObj.goods_name,
+  //         goods_price:_goodsObj.goods_price,
+  //         pics: _goodsObj.pics,
+  //         goods_introduce:_goodsObj.goods_introduce.replace(/\.webp/g,'.jpg')
+  //       }
+  //     })
+  //   })
+      const goodsObj = (await request({url:'/goods/detail', data:{goods_id}})).data.message;
+      this.GoodsInfo = goodsObj;
+      // 获取缓存中的商品收藏的数组
+      let collect = wx.getStorageSync('collect')||[];
+      let isCollect = collect.some(v=>v.goods_id===this.GoodsInfo.goods_id);
       this.setData({
         goodsObj:{
-          goods_name:_goodsObj.goods_name,
-          goods_price:_goodsObj.goods_price,
-          pics: _goodsObj.pics,
-          goods_introduce:_goodsObj.goods_introduce.replace(/\.webp/g,'.jpg')
-        }
+          goods_name:goodsObj.goods_name,
+          goods_price:goodsObj.goods_price,
+          pics: goodsObj.pics,
+          goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg')
+        },
+        isCollect
       })
-    })
   },
   // 轮播图预览
   handlePreviewImage(e){
@@ -51,6 +68,9 @@ Page({
       current,
       urls
     })
+   
+    
+    
   },
   // 加入购物车
   handleJoinCart(e){
@@ -69,6 +89,31 @@ Page({
       icon: 'success',
       mask:true
     })
+  },
+  // 点击收藏
+  handleCollect(){
+    let isCollect = false;
+    let collect = wx.getStorageSync('collect')||[];
+    let index = collect.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    if(index!==-1){
+      collect.splice(index, 1);
+      isCollect = false;
+      wx.showToast({
+        title: '取消成功',
+        icon:'success',
+        mask:true
+      })
+    }else{
+      collect.push(this.GoodsInfo);
+      isCollect = true;
+      wx.showToast({
+        title: '收藏成功',
+        icon:'success',
+        mask:true
+      })
+    }
+    wx.setStorageSync('collect', collect);
+    this.setData({isCollect});
   },
 
   
